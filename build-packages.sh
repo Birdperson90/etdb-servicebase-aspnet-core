6138b2ac7ab7c073698934b7291818eaa477c666
Birdperson90 <al.khatib.sami1990@gmail.com>
2018-04-13 23:30:44 +0200

    raise version numbers

--- build-packages.sh
+++ build-packages.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+echo "path is $1";
+if [ ! -d "$1" ]; then
+    echo "invalid path, exiting!";
+    exit;
+fi
 cd src/
-for d in */; do cd ${d} && dotnet pack *.csproj --include-symbols --output /home/firebirdy/Shared/Nuget/ && cd ..; done
+for d in */; do cd ${d} && dotnet pack *.csproj --include-symbols --output "$1" && cd ..; done
