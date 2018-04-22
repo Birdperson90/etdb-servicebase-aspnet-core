f0d692087b1b1c4068abf589f9aa115c04c08587
Birdperson90 <al.khatib.sami1990@gmail.com>
2018-04-22 21:56:12 +0200

    update package buildscript; update runtime version for xunit tests

--- build-packages.sh
+++ build-packages.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-echo "path is $1";
-if [ ! -d "$1" ]; then
-    echo "invalid path, exiting!";
-    exit;
-fi
 cd src/
-for d in */; do cd ${d} && dotnet pack *.csproj --include-symbols --output "$1" && cd ..; done
+for d in */; do 
+    if [ -d ${d} ]; then
+        cd ${d} && dotnet pack *.csproj --include-symbols --output "../../../nuget/" && cd ..; 
+    fi
+done
