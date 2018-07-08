e59b25a79afade8e7b268194687f32ff5ba9e9ff
alsami <al.khatib.sami1990@gmail.com>
2018-07-08 13:30:37 +0200

    refacotiring

--- src/Etdb.ServiceBase.Builder/Modules/CqrsModule.cs
+++ src/Etdb.ServiceBase.Builder/Modules/CqrsModule.cs
@@ -5,6 +5,7 @@ using Etdb.ServiceBase.Cqrs.Abstractions.Bus;
 using Etdb.ServiceBase.Cqrs.Abstractions.Validation;
 using Etdb.ServiceBase.Cqrs.Bus;
 using MediatR;
+using MediatR.Extensions.Autofac.DependencyInjection;
 using MediatR.Pipeline;
 
 namespace Etdb.ServiceBase.Builder.Modules
@@ -20,41 +21,15 @@ namespace Etdb.ServiceBase.Builder.Modules
 
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterAssemblyTypes(typeof(IMediator).Assembly)
-                .AsImplementedInterfaces();
-
+            builder.AddMediatR(this.assembliesToScan);
+            
             builder.RegisterType<Bus>()
                 .As<IBus>()
-                .AsSelf()
                 .InstancePerLifetimeScope();
-
-            var openGenericTypes = new[]
-            {
-                typeof(IRequestHandler<>),
-                typeof(IRequestHandler<,>),
-                typeof(INotificationHandler<>),
-                typeof(ICommandValidation<>),
-            };
-
-            foreach (var openGenericType in openGenericTypes)
-            {
-                builder.RegisterAssemblyTypes(this.assembliesToScan)
-                    .AsClosedTypesOf(openGenericType)
-                    .AsImplementedInterfaces();
-            }
-
-            builder.RegisterGeneric(typeof(RequestPostProcessorBehavior<,>))
-                .As(typeof(IPipelineBehavior<,>));
-
-            builder.RegisterGeneric(typeof(RequestPreProcessorBehavior<,>))
-                .As(typeof(IPipelineBehavior<,>));
-
-            builder.Register<ServiceFactory>(ctx =>
-            {
-                var componentContext = ctx.Resolve<IComponentContext>();
-
-                return type => componentContext.Resolve(type);
-            });
+            
+            builder.RegisterAssemblyTypes(this.assembliesToScan)
+                .AsClosedTypesOf(typeof(ICommandValidation<>))
+                .AsImplementedInterfaces();
         }
     }
 }
