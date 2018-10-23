3975e6d82efcec0fa99990c5560e9e2adcffb661
Sami <Al-Khatib.Sami@fronius.com>
2018-10-23 16:10:06 +0200

    add resourcelocked exception and filter and raise package versions

---
+++ src/Etdb.ServiceBase.Exceptions/ResourceLockedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etdb.ServiceBase.Exceptions
+{
+    public class ResourceLockedException : Exception
+    {
+        public Type ResourceType { get; set; }
+        public object ResourceKey { get; set; }
+
+        public ResourceLockedException(Type resourceType, object resourceKey,
+            string message = "The resource is currently busy!") : base(message)
+        {
+            this.ResourceType = resourceType;
+            this.ResourceKey = resourceKey;
+        }
+    }
+}
---
+++ src/Etdb.ServiceBase.Filter/ResourceLockedExceptionFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Etdb.ServiceBase.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Etdb.ServiceBase.Filter
+{
+    public class ResourceLockedExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ResourceLockedExceptionFilter> logger;
+
+        public ResourceLockedExceptionFilter(ILogger<ResourceLockedExceptionFilter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (!(context.Exception is ResourceLockedException exception))
+            {
+                return;
+            }
+
+            this.logger.LogError(exception,
+                $"The resource of type {exception.ResourceType.Name} with key {exception.ResourceKey} was busy at {DateTime.UtcNow}");
+
+            context.ExceptionHandled = true;
+            context.HttpContext.Response.StatusCode = (int) HttpStatusCode.Conflict;
+            context.Result = new ObjectResult(new
+            {
+                exception.Message
+            });
+        }
+    }
+}
