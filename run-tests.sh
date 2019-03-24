477440cf5f645aa3b12cd75c43479d5d6c38afaa
alsami <al.khatib.sami1990@gmail.com>
2019-03-24 09:09:34 +0100

    update buildscripts

--- run-tests.sh
+++ run-tests.sh
@@ -4,6 +4,10 @@ for d in test/*;
         if [[ ${d} == *Tests ]]; then
             cd ${d} 
             dotnet test
+            if [[ ${?} != 0  ]]
+            then
+                exit -1
+            fi
             cd ../../;
         fi 
 done
