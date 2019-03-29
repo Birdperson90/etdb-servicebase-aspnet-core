ec9c24c2a852c483d575517bf7936337999b5f0a
alsami <al.khatib.sami1990@gmail.com>
2019-03-29 08:28:45 +0100

    update publish-script

--- publish-packages.sh
+++ publish-packages.sh
@@ -3,7 +3,8 @@ cd src
 for directory in *; do 
     if [[ -d ${directory} ]]; then
         cd ${directory}
-        dotnet pack *.csproj --include-symbols --output "."
+        dotnet build -c Release
+        dotnet pack *.csproj --include-symbols -c Release --output "."
         dotnet nuget push -s ${1} -k ${2} "${directory}.${3}.symbols.nupkg"
         if [[ ${?} != 0  ]]
         then
