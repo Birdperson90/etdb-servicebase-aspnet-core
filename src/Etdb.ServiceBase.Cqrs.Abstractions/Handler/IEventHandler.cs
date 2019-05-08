bee04105fad024a32acd3f209056785c40509c53
Sami <Al-Khatib.Sami@fronius.com>
2019-05-08 11:35:22 +0200

    rename `IMessageHandler` to `IEventHandler`

--- src/Etdb.ServiceBase.Cqrs.Abstractions/Handler/IEventHandler.cs
+++ src/Etdb.ServiceBase.Cqrs.Abstractions/Handler/IEventHandler.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Etdb.ServiceBase.Cqrs.Abstractions.Handler
 {
-    public interface IMessageHandler<in TMessage> : INotificationHandler<TMessage> where TMessage : IEvent
+    public interface IEventHandler<in TMessage> : INotificationHandler<TMessage> where TMessage : IEvent
     {
     }
 }
