10db43fb570431ac9a99a1c4fd3e9569d742cafc
Sami <Al-Khatib.Sami@fronius.com>
2019-05-09 12:20:41 +0200

    Add ApplicationUser to Services library and raise version

---
+++ src/Etdb.ServiceBase.Services.Abstractions/IApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Etdb.ServiceBase.Services.Abstractions
+{
+    public interface IApplicationUser
+    {
+        Guid Id { get; }
+
+        string UserName { get; }
+
+        bool IsAuthenticated();
+    }
+}
---
+++ src/Etdb.ServiceBase.Services/ApplicationUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Etdb.ServiceBase.Services.Abstractions;
+using IdentityModel;
+using Microsoft.AspNetCore.Http;
+
+namespace Etdb.ServiceBase.Services
+{
+    public class ApplicationUser : IApplicationUser
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public ApplicationUser(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid Id => GetUserId();
+        public string UserName => GetUserName();
+
+        private string GetUserName()
+        {
+            if (!this.IsAuthenticated())
+            {
+                return string.Empty;
+            }
+
+            return this.httpContextAccessor.HttpContext.User.Claims
+                .First(claim => claim.Type == JwtClaimTypes.PreferredUserName).Value;
+        }
+
+        public bool IsAuthenticated() => this.httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+
+        private Guid GetUserId()
+        {
+            if (!this.IsAuthenticated())
+            {
+                return Guid.Empty;
+            }
+
+            var idClaimValue = this.httpContextAccessor.HttpContext.User.Claims
+                .First(claim => claim.Type == JwtClaimTypes.Subject).Value;
+
+            return Guid.Parse(idClaimValue);
+        }
+    }
+}
