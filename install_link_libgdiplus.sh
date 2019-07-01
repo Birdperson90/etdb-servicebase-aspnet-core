d7d51de946e2edf36b478480b29bc2dcb6642860
alsami <al.khatib.sami1990@gmail.com>
2019-07-01 20:06:32 +0200

    chore: add comments to weird build-script

--- install_link_libgdiplus.sh
+++ install_link_libgdiplus.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+# This script is required for System.Drawing.Common
+# For more information check out this answer of an github issue
+# https://github.com/JanKallman/EPPlus/issues/83#issuecomment-404570402
 sudo ln -s /lib/x86_64-linux-gnu/libdl.so.2 /lib/x86_64-linux-gnu/libdl.so
 sudo apt update
 sudo apt install -y libgdiplus
